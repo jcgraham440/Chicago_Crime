fcd1e929da29b655ad5e3b9989d1d5852044f8ea
Carly Russell <c.a.movius@gmail.com>
2019-10-24 22:38:31 -0700

    add static/js/config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,4 @@
 // API key
 const API_KEY = "pk.eyJ1IjoiamNncmFoYW00NDAiLCJhIjoiY2swZWtudmlvMDBoeTNvbzFhNDJhd2FheiJ9.8z7Wld8t7PsyrTRBTrMZ3g";
+
+const API_KEY_2 = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
